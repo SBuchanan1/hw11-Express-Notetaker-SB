8d5969396ea60b3ef9a1a8002748058d435e80da
SBuchanan1 <syd.buch@gmail.com>
2020-11-12 06:12:29 -0500

    deleted and added A LOT of files

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // Dependencies
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const db = require("./db/db.json")
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+// const fs = require("fs");
+// const path = require("path");
+// const db = require("./db/db.json")
 
 
 // Var for Express
 const app = express();
 
 // PORT for incoming request
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Setup for Express data parsing/Middleware
 const rootObj = { root: __dirname + "/public" };
@@ -18,42 +20,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use(apiRoutes);
-app.use(htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.get("/", (res, req) => res.sendFile("/index.html", rootObj));
+// Server the app is listening on
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
-app.get("/notes", (req, res) => res.sendFile("/notes.html", rootObj));
-app.get("/api/notes", (req, res) => {
-    // console.log("/api/notesget")
-    let json = getJson();
-    console.log(json);
-    res.json(json);
-})
+// app.get("/", (res, req) => res.sendFile("/index.html", rootObj));
 
-app.post("/api/notes", (req, res) => {
-    // console.log("./api/notespost")
-    // console.log(req.body);
-    addNoteToJSON(req.body)
-    res.json(getJson());
-})
+// app.get("/notes", (req, res) => res.sendFile("/notes.html", rootObj));
+// app.get("/api/notes", (req, res) => {
+//     // console.log("/api/notesget")
+//     let json = getJson();
+//     console.log(json);
+//     res.json(json);
+// })
 
-app.delete("/api/notes/id:", (req, res) => {
-    // console.log("/api/notes/id:delete")
-    deleteNoteToJSON(req.params.id);
-    res.json(getJson());
-})
+// app.post("/api/notes", (req, res) => {
+//     // console.log("./api/notespost")
+//     // console.log(req.body);
+//     addNoteToJSON(req.body)
+//     res.json(getJson());
+// })
 
-// Inherting files
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// app.delete("/api/notes/id:", (req, res) => {
+//     // console.log("/api/notes/id:delete")
+//     deleteNoteToJSON(req.params.id);
+//     res.json(getJson());
+// })
 
-// Server request & console log 
-app.listen(PORT, function () {
-    console.log("App is listening on: http://localhost" + PORT);
-});
+// // Inherting files
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
-function getJson() {
-    let data = fs.readFileSync(_dirname = "/db/db.json", data);
+// // Server request & console log 
+// app.listen(PORT, function () {
+//     console.log("App is listening on: http://localhost" + PORT);
+// });
 
-}
+// function getJson() {
+//     let data = fs.readFileSync(_dirname = "/db/db.json", data);
+
+// }
