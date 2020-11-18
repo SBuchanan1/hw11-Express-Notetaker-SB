f90734ae1ea2c484098f1e5231799a4cf7cdc9bd
SBuchanan1 <syd.buch@gmail.com>
2020-11-18 13:00:13 -0500

    modified syntax in server.js

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Needed to add this middleware for css to work (https://stackoverflow.com/questions/13395742/can-not-get-css-file)
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// have to put API routes before HTML routes, because HTML routes contains * (catch-all)
+// have to put API routes before HTML route
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
-})
+});
